3ccd714da7784822dc1d84fdbab59926038ca60b
Dangthetai582 <50923513+Dangthetai582@users.noreply.github.com>
2019-05-29 15:28:48 +0700

    Add files via upload

---
+++ 3.10.py
@@ -0,0 +1,6 @@
+import math
+r=float(input("Nhap ban kinh duong tron:"))
+c=2*math.pi*r
+s=math.pi*r*r
+print("chu vi ban kinh duong tron {0} la {1}".format(r,c))
+print("dien tich ban kinh duong tron {0} la {1}".format(r,s))
